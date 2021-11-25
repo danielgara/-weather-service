2fc121ddd478af141cc0ffa1da37f0d5b436f89b
Daniel Correa <yo@danielgara.com>
2021-11-25 08:45:46 -0500

    imp: quick fix

--- src/main/java/com/eafit/weather/Services/WeatherService.java
+++ src/main/java/com/eafit/weather/Services/WeatherService.java
@@ -9,14 +9,18 @@ public class WeatherService {
 
     @GetMapping("/weather")
     public String getWeather() {
-        //Here we call the API (which can be a method of another class)
+        //Here we start the timer.
+        //Here we call the API (which can be a method of another class).
+        //Here we stop the timer.
         //Here we store in mongo
         return "Here return the json";
     }
 
     @GetMapping("/weather/{location}")
     public String getWeatherLocation(@PathVariable String location) {
-        //Here we call the API (which can be a method of another class)
+        //Here we start the timer.
+        //Here we call the API (which can be a method of another class).
+        //Here we stop the timer.
         //Here we store in mongo
         return "Here return the json based on location";
     }
